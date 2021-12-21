76b2794e9db57a3d6ff6204e14eaac915c6b1712
anandkumarcoder <akanand104@gmail.com>
2021-12-21 23:39:36 +0530

    navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,42 @@
 import React from 'react'
 import '../styles/Navbar.css'
-import {logo} from "../data/data.json"
-
+import  data  from "../data/data.json"
 import {Link} from "react-router-dom"
+
+const searchIcon = <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
 
 const Navbar = () => {
     return (
         <div className='nav'>
             <div className='logo'>
                 <Link to="/dsd">
-                    <img id='logoImage' src={logo} alt="Not" />
+                    <img id='logoImage' src={data.logo} alt="Not" />
                 </Link>
-                
+                </div>
+
                 <Link className="navLinks" to="/miphones">Mi Phones
                 </Link>
                 <Link className="navLinks" to="/tv">TV
                 </Link>
                 <Link className="navLinks" to="/laptops">Laptops
                 </Link>
-                <Link className="navLinks" to="/miphones">Mi Phones
+                <Link className="navLinks" to="/lifestyle">Lifestyle & fitness
                 </Link>
-                <Link className="navLinks" to="/miphones">Mi Phones
+                <Link className="navLinks" to="/home">Home
                 </Link>
-                <Link className="navLinks" to="/miphones">Mi Phones
+                <Link className="navLinks" to="/audio">Radio
                 </Link>
-                <Link className="navLinks" to="/miphones">Mi Phones
+                <Link className="navLinks" to="/accessories">Accessories
                 </Link>
+
+                <div className='searchBox'>
+                    <input type="text" name='search' placeholder='search products' /> {searchIcon}
+
+                </div>
 
 
 
-            </div>
+           
             
         </div>
     )
