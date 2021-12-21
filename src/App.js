ebce9d0ced75f61279cdd348e208c1ef8d62c968
anandkumarcoder <akanand104@gmail.com>
2021-12-22 00:26:11 +0530

    slider completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import PreNavbar from './components/PreNavbar';
-import Navbar from './components/Navbar.js'
-import{BrowserRouter as Router} from "react-router-dom"
+import Navbar from './components/Navbar.js';
+import{BrowserRouter as Router} from "react-router-dom";
+import Slider from "./components/Slider.js"
+import data from "./data/data.json"
 
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <Router>
       <PreNavbar/>
       <Navbar/>
+      <Slider start={data.banner.start}/>
       </Router>
     // this is jsx(combination of js and html)
   );
