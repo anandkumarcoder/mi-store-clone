b3e64e16f8cda1e9028a2323afdd63207b2f5392
anandkumarcoder <akanand104@gmail.com>
2021-12-21 09:47:58 +0530

    Nav bar structurenstarted

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../styles/Navbar.css'
+import {Link} from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <div className='nav'>
+            <div className='logo'>
+                <Link to="">
+                    <img id='logoImage' src={} alt="Not" />
+                </Link>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Navbar
