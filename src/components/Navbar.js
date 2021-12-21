f8f6ce95dcc98a38908417f5ae93969357883b86
anandkumarcoder <akanand104@gmail.com>
2021-12-21 18:47:33 +0530

    logo export problem

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import '../styles/Navbar.css'
+import {logo} from "../data/data.json"
+
 import {Link} from "react-router-dom"
 
 const Navbar = () => {
     return (
         <div className='nav'>
             <div className='logo'>
-                <Link to="">
-                    <img id='logoImage' src={} alt="Not" />
+                <Link to="/dsd">
+                    <img id='logoImage' src={logo} alt="Not" />
                 </Link>
+                
+                <Link className="navLinks" to="/miphones">Mi Phones
+                </Link>
+                <Link className="navLinks" to="/tv">TV
+                </Link>
+                <Link className="navLinks" to="/laptops">Laptops
+                </Link>
+                <Link className="navLinks" to="/miphones">Mi Phones
+                </Link>
+                <Link className="navLinks" to="/miphones">Mi Phones
+                </Link>
+                <Link className="navLinks" to="/miphones">Mi Phones
+                </Link>
+                <Link className="navLinks" to="/miphones">Mi Phones
+                </Link>
+
+
 
             </div>
             
