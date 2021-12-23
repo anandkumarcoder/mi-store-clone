55d187d1b25f5a149a06c08ed057d8eb564fb5aa
anandkumarcoder <akanand104@gmail.com>
2021-12-23 13:53:01 +0530

    offer banner completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar.js';
 import{BrowserRouter as Router} from "react-router-dom";
 import Slider from "./components/Slider.js"
 import data from "./data/data.json"
+import Offers from "./components/Offers.js"
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <PreNavbar/>
       <Navbar/>
       <Slider start={data.banner.start}/>
+      <Offers offer={data.offer} />
       </Router>
     // this is jsx(combination of js and html)
   );
