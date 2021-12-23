fdf9ccb3d8dc3279e0ccbd518c866a15ed735b25
anandkumarcoder <akanand104@gmail.com>
2021-12-24 01:53:19 +0530

    star product cards view fixed

--- src/components/StarProduct.js
+++ src/components/StarProduct.js
@@ -10,7 +10,6 @@ const StarProduct = ({starProduct}) => {
             <div>
             <a href={starProduct[1].url}> <img src={starProduct[1].image} alt="2nd product" /></a> 
             <a href={starProduct[2].url}> <img src={starProduct[2].image} alt="3rd product" /></a> 
-            
             <a href={starProduct[3].url}> <img src={starProduct[3].image} alt="4th product" /></a>
             </div>
             
