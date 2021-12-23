aac4d8c3fc6cc0c348fd166084f4409376679d8f
anandkumarcoder <akanand104@gmail.com>
2021-12-23 14:32:11 +0530

    Header & star product done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import{BrowserRouter as Router} from "react-router-dom";
 import Slider from "./components/Slider.js"
 import data from "./data/data.json"
 import Offers from "./components/Offers.js"
+import Heading from "./components/Heading.js"
+import StarProduct from "./components/StarProduct.js"
+
 
 
 function App() {
@@ -14,6 +17,8 @@ function App() {
       <Navbar/>
       <Slider start={data.banner.start}/>
       <Offers offer={data.offer} />
+      <Heading text= "STAR PRODUCTS" />
+      <StarProduct starProduct ={data.starProduct} />
       </Router>
     // this is jsx(combination of js and html)
   );
