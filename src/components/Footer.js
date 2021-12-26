19c38c389bac4f1e8e92a0ad7847666ae844dd6e
anandkumarcoder <akanand104@gmail.com>
2021-12-26 12:51:14 +0530

    Footer CSS done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../styles/PreFooter.css"
+import "../styles/Footer.css"
 
 
 const repeatIcon = <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z"/></svg>
@@ -63,45 +64,52 @@ const Footer = ({footer}) => {
 
             <div className='footer'>
                 <div>
-                    <p>Support</p>
+                    <p>SUPPORT</p>
                     {footer.support.map((item,index)=> (
                         <a key={item.url} href={item.url}>{item.name}</a>
                     ))}
                 </div>
 
                 <div>
-                    <p>Shop & Learn</p>
+                    <p>SHOP & LEARN</p>
                     {footer.shopAndLearn.map((item,index)=> (
                         <a key={item.url} href={item.url}>{item.name}</a>
                     ))}
                 </div>
 
                 <div>
-                    <p>Retail Store</p>
+                    <p>RETAIL STORE</p>
                     {footer.retailStore.map((item,index)=> (
                         <a key={item.url} href={item.url}>{item.name}</a>
                     ))}
                 </div>
 
                 <div>
-                    <p>About US</p>
+                    <p>ABOUT US</p>
                     {footer.aboutUS.map((item,index)=> (
                         <a key={item.url} href={item.url}>{item.name}</a>
                     ))}
                 </div>
 
                 <div>
-                    <p>Contact US</p>
+                    <p>CONTACT US</p>
                     {footer.contactUs.map((item,index)=> (
                         <a key={item.url} href={item.url}>{item.name}</a>
                     ))}
                 </div>
+                <div>
+                      <div>Chat with our Virtual AI Bot(24/7 Live Agent Support)</div><button>CHAT NOW</button>
+                  </div>
 
             </div>
+
+            
+
 
             <div className='footerBorder'>
                 <div>
                     Copyright © 2010-2021 Xiaomi. All rights reserved
+                    
                 </div>
 
             </div>
